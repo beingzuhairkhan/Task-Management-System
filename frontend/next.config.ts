89804fd669645fe3eca5014362945744cc91aa95
beingzuhairkhan <zuhairkhan5134@gmail.com>
2026-08-12 16:16:14 +0530

    login flow change

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
